4b3b2c90b13ae48ead8cc620347091d2c0e59df5
liucong <liucong07@gmail.com>
2009-05-31 22:50:27 +0800

    a new commit.

---
+++ rt/transports/jms-spec/src/main/java/org/apache/cxf/transport/jms/uri/spec/JMSSpecConstants.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.transport.jms.uri.spec;
+
+/**
+ * 
+ */
+public class JMSSpecConstants {
+    
+    public static final String QUEUE_PREFIX = "queue:";
+    public static final String TOPIC_PREFIX = "topic:";
+    public static final String JNDI_PREFIX = "jndi:";
+
+    public static final String DELIVERYMODE_PERSISTENT = "PERSISTENT";
+    public static final String DELIVERYMODE_NON_PERSISTENT = "NON_PERSISTENT";
+    
+    //shared parameters
+    public static final String DELIVERYMODE_PARAMETER_NAME = "deliveryMode";
+    public static final String TIMETOLIVE_PARAMETER_NAME = "timeToLive";
+    public static final String PRIORITY_PARAMETER_NAME = "priority";
+    public static final String REPLYTONAME_PARAMETER_NAME = "replyToName";
+    
+    //default parameters
+    public static final String DELIVERYMODE_DEFAULT = DELIVERYMODE_NON_PERSISTENT;
+    public static final int TIMETOLIVE_DEFAULT = -1;
+    public static final int PRIORITY_DEFAULT = 5;
+    
+    //jndi parameters ? need to be sure.
+    public static final String JNDICONNECTIONFACTORYNAME_PARAMETER_NAME = "jndiConnectionFactoryName";
+    public static final String JNDIINITIALCONTEXTFACTORY_PARAMETER_NAME = "jndiInitialContextFactory";
+    public static final String JNDIURL_PARAMETER_NAME = "jndiURL";
+    public static final String JNDI_PREFIX_PARAMETER_NAME = "jndi-";
+    
+    //queue and topic parameters
+    public static final String TOPICREPLYTONAME_PARAMETER_NAME = "topicReplyToName";
+    
+    public JMSSpecConstants() {
+    }
+    
+    public void get() {
+        
+    }
+}
