e7a6051a7b5eae691867109575bc1884761a20e0
liucong <liucong07@gmail.com>
2009-07-20 11:31:19 +0800

    update from svn.

--- systests/src/test/java/org/apache/cxf/systest/jaxrs/security/SecureBookStoreNoInterface.java
+++ systests/src/test/java/org/apache/cxf/systest/jaxrs/security/SecureBookStoreNoInterface.java
@@ -22,7 +22,9 @@ package org.apache.cxf.systest.jaxrs.security;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,6 +42,16 @@ public class SecureBookStoreNoInterface {
         book.setId(123L);
         book.setName("CXF in Action");
         books.put(book.getId(), book);
+    }
+    
+    @POST
+    @Path("/bookforms")
+    @Secured({"ROLE_USER", "ROLE_ADMIN" })
+    public Book getBookFromFormParams(@FormParam("name") String name, @FormParam("id") long id) {
+        if (name == null || id == 0) {
+            throw new RuntimeException("FormParams are not set");
+        }
+        return new Book(name, id);
     }
     
     @GET
@@ -67,5 +79,10 @@ public class SecureBookStoreNoInterface {
     @Secured("ROLE_ADMIN")
     public Book getThatBook() throws BookNotFoundFault {
         return books.get(123L);
+    }
+    
+    @Path("/securebook")
+    public SecureBook getSecureBook() throws BookNotFoundFault {
+        return new SecureBook("CXF in Action", 123L);
     }
 }
